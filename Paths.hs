b3549a1db6e4607976effc55768f28d2fe96b3cf
Neil Mitchell <ndmitchell@gmail.com>
2014-12-20 23:20:56 +0000

    GHC 7.10 fix to avoid using Version directly

--- Paths.hs
+++ Paths.hs
@@ -5,6 +5,7 @@ module Paths_js_jquery where
 import Data.Version
 import System.Directory
 import System.FilePath
+import Text.ParserCombinators.ReadP
 
 
 getDataFileName :: FilePath -> IO FilePath
@@ -13,4 +14,5 @@ getDataFileName x = do
     return $ dir </> "javascript" </> x
 
 version :: Version
-version = Version [1,11,2] []
+-- can't write a literal Version value since in GHC 7.10 the versionsTag field is deprecated
+version = head $ [v | (v,"") <- readP_to_S parseVersion "1.11.2"] ++ error "version, failed to parse"
