36f177ba8c73352f6ff649f74fbdc8cfb5163c90
Neil Mitchell <ndmitchell@gmail.com>
2025-02-09 22:28:54 +0000

    jQuery 3.7.1
    Summary:
    
    Test Plan:

--- src/Paths.hs
+++ src/Paths.hs
@@ -1,12 +1,10 @@
 -- | Fake cabal module for local building
-
-module Paths_js_jquery(getDataFileName, version) where
+module Paths_js_jquery (getDataFileName, version) where
 
 import Data.Version
 import System.Directory
 import System.FilePath
 import Text.ParserCombinators.ReadP
-
 
 getDataFileName :: FilePath -> IO FilePath
 getDataFileName x = do
@@ -15,4 +13,4 @@ getDataFileName x = do
 
 version :: Version
 -- can't write a literal Version value since in GHC 7.10 the versionsTag field is deprecated
-version = head $ [v | (v,"") <- readP_to_S parseVersion "3.3.1"] ++ error "version, failed to parse"
+version = head $ [v | (v, "") <- readP_to_S parseVersion "3.7.1"] ++ error "version, failed to parse"
